9ae1d29ad3fda3d140662ee89c1d3f3a409a52f3
Sebastian Kalinowski <sebastian@kalinowski.eu>
2015-10-06 16:26:22 +0200

    Implement basic API facilities
    Added code that allow to implement API endpoints with Pecan as a
    "framework". Example test was also added.
    Integrated oslo.log to enable logging.
    Dev server can be run by installing bareon_api (for example using pip)
    and then calling bin/bareon-api script.

---
+++ bareon_api/api/controllers/root.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import pecan
+
+from bareon_api.common import utils
+
+
+LOG = utils.getLogger(__name__)
+
+
+class V1Controller(object):
+    """Root controller for the v1 API"""
+
+    def __init__(self):
+        LOG.debug('=== Creating V1Controller ===')
+
+    @pecan.expose(generic=True)
+    def index(self):
+        pecan.abort(405)  # HTTP 405 Method Not Allowed as default
+
+    @index.when(method='GET', template='json')
+    def on_get(self):
+        return {'message': 'hello world'}
--- bareon_api/tests/base.py
+++ bareon_api/tests/base.py
@@ -16,8 +16,29 @@
 # under the License.
 
 from oslotest import base
+import pecan
+import webtest
+
+from bareon_api.api.controllers import root as root_ctrl
 
 
 class TestCase(base.BaseTestCase):
-
     """Test case base class for all unit tests."""
+
+
+class FunctionalTestCase(TestCase):
+
+    def setUp(self):
+        super(FunctionalTestCase, self).setUp()
+        root = self.root
+        config = {'app': {'root': root}}
+        pecan.set_config(config, overwrite=True)
+        self.app = webtest.TestApp(pecan.make_app(root))
+
+    def tearDown(self):
+        super(FunctionalTestCase, self).tearDown()
+        pecan.set_config({}, overwrite=True)
+
+    @property
+    def root(self):
+        return root_ctrl.V1Controller()
