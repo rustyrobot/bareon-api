c8f89788a38d677e68511dcdd71bbfa83a1c9ab2
Sebastian Kalinowski <sebastian@kalinowski.eu>
2015-10-27 13:10:46 +0100

    Dummy way to use node disks data

--- bareon_api/common/config.py
+++ bareon_api/common/config.py
@@ -20,11 +20,54 @@ from oslo_config import cfg
 from oslo_log import log
 
 
+API_SERVICE_OPTS = [
+    cfg.StrOpt('host_ip',
+               default='0.0.0.0',
+               help='The IP address on which api listens.'),
+    cfg.IntOpt('port',
+               default=9322,
+               min=1, max=65535,
+               help='The TCP port on which api listens.'),
+]
+
+DISCOVERY_SERVICE_OPTS = [
+    cfg.StrOpt('discovery_host_ip',
+               default='0.0.0.0',
+               help='The IP address on which discovery api listens.'),
+    cfg.IntOpt('discovery_port',
+               default=8881,
+               min=1, max=65535,
+               help='The TCP port on which dsiscovry api listens.'),
+]
+
+opt_group = cfg.OptGroup(
+    name='api',
+    title='Options for the api service')
+
+
 def make_config():
     conf = cfg.ConfigOpts()
+
+    conf.register_group(opt_group)
+
+    conf.register_opts(API_SERVICE_OPTS, opt_group)
+    conf.register_cli_opts(API_SERVICE_OPTS)
+
+    conf.register_opts(DISCOVERY_SERVICE_OPTS, opt_group)
+    conf.register_cli_opts(DISCOVERY_SERVICE_OPTS)
+
     log.register_options(conf)
     return conf
 
 
+def parse_args(conf, args=None):
+    import sys
+
+    conf(args=args if args else sys.argv[1:],
+         project='bareon-api',
+         version='1.0.0')
+
+
 CONF = make_config()
 logging.getLogger(__name__)
+parse_args(CONF)
--- bareon_api/models.py
+++ bareon_api/models.py
@@ -17,6 +17,9 @@ from copy import deepcopy
 import random
 
 from fuel_agent import objects
+import requests
+
+from bareon_api.common.config import CONF
 
 
 class JsonifyMixin(object):
@@ -50,28 +53,6 @@ class VolumeGroup(JsonifyMixin, objects.VG):
     pass
 
 
-DISKS_TEMPLATE = {
-    'disk': 'disk/by-path/pci-0000:00:0b.0-virtio-pci-virtio2',
-    'extra': ['disk/by-id/virtio-8140d936d3ee4b04a6b5'],
-    'model': None,
-    'name': 'sda',
-    # TODO(rustyrobot) this information mustn't be hardcoded in the agent [1],
-    # instead discovery should provide all required data as is
-    # so after that service can determine which device is really
-    # removable
-    #
-    # [1] https://github.com/openstack/fuel-nailgun-agent/blob
-    #                       /abab45cf8c7344d43acd3858c02d7a648ef7fee6/agent#L41-L52
-    'removable': '0',
-    # TODO(rustyrobot figure out what we should use internally, probably
-    # megabytes, even if discovery provides bytes [1]
-    #
-    # [1] https://github.com/openstack/fuel-web/blob
-    #                       /93a42ebbde6ecf8c5e36cdec06e59aa70e5f044b/nailgun/nailgun/objects/node.py#L349-L360
-    'size': 40000
-}
-
-
 FS = {
     # ???(prmtl) now we identify file system by lablel
     # but should be considered to find a better way
@@ -83,57 +64,64 @@ FS = {
     ),
 }
 
-LV = {
-    'kura': LogicalVolume(
-        name='kura',
-        size=69,
-        vgname='kurnik'
-    ),
-    'kogut': LogicalVolume(
-        name='kogut',
-        size=96,
-        vgname='kurnik'
-    )
-}
 
-VG = {
-    'kurnik': VolumeGroup(
-        name='kurnik',
-        pvnames=[
-            '/dev/vdc1',
-        ],
-    )
-}
-
-PARTITION = {
-    'vdc': {
-        'vdc1': Partition(
-            name='/dev/vdc1',
-            device='/dev/vdc',
-            count=1,
-            partition_type='primary',
-            begin=1,
-            end=20000,
+def make_lv(vg):
+    return {
+        'kura': LogicalVolume(
+            name='kura',
+            size=69,
+            vgname=vg.name,
+        ),
+        'kogut': LogicalVolume(
+            name='kogut',
+            size=96,
+            vgname=vg.name,
         )
     }
-}
-
-PARTED = {
-    'vdc': Parted(
-        label='gpt',
-        name='/dev/vdc',
-        partitions=PARTITION['vdc'].values()
-    )
-}
 
 
-PV = {
-    'vdc1': PhysicalVolume(
-        name='/dev/vdc1',
-        metadatacopies=2,
-        metadatasize=28,
-    )
-}
+def make_vg(pvs):
+    return {
+        'kurnik': VolumeGroup(
+            name='kurnik',
+            pvnames=[pv.name for pv in pvs],
+        )
+    }
+
+
+def make_parted_and_partitions(disk):
+    device = disk['device']
+    partitions = {
+        device: {
+            '{0}1'.format(device): Partition(
+                name='/dev/{0}1'.format(device),
+                device='/dev/{0}'.format(device),
+                count=1,
+                partition_type='primary',
+                begin=1,
+                end=20000,
+            )
+        }
+    }
+    parted = {
+        disk['device']: Parted(
+            label='gpt',
+            name='/dev/{0}'.format(disk['device']),
+            partitions=partitions[device].values()
+        )
+
+    }
+    return parted, partitions
+
+
+def make_pv(disk):
+    return {
+        '{}1'.format(disk['device']): PhysicalVolume(
+            name='/dev/{0}1'.format(disk['device']),
+            metadatacopies=2,
+            metadatasize=28,
+        )
+    }
 
 
 def random_mac():
@@ -146,27 +134,45 @@ def random_mac():
     return ':'.join(map(lambda x: "%02x" % x, mac))
 
 
-def generate_nodes_disks(count):
+def get_nodes_and_disks():
+
+    # dummy way to check if this is a disk
+    # it should be added as a info to the ohai data
+    # during the discovery
+    def is_disk(block_device):
+        return block_device.get('vendor') in ['ATA', ]
+
+    def get_nodes_discovery_data():
+        discovery_url = 'http://{ip}:{port}/'.format(
+            ip=CONF.discovery_host_ip,
+            port=CONF.discovery_port,
+        )
+        resp = requests.get(discovery_url)
+        return resp.json()
+
+    def filter_disks(block_devices):
+        disks = []
+        for dev, info in block_devices.items():
+            if is_disk(info):
+                info['device'] = dev
+                disks.append(info)
+        return disks
+
     nodes = {}
     disks = {}
-    for _ in xrange(count):
-        mac = random_mac()
-
-        disks[mac] = [deepcopy(DISKS_TEMPLATE)]
-        nodes[mac] = {
-            'host': 'some-host',
-            'ssh_key': 'some-ssh-key',
-            'disks': disks[mac],
+    for node in get_nodes_discovery_data():
+        disks[node['mac']] = filter_disks(node['discovery']['block_device'])
+        nodes[node['mac']] = {
+            'disks': disks[node['mac']],
             # NOTE(prmtl): it really doesn't matter if it's mac
             # or anything as long as it is consistent between
             # services in PoC
-            'id': mac,
+            'id': node['mac'],
         }
-
     return nodes, disks
 
 
-def generate_spaces(nodes):
+def generate_spaces(nodes, disks):
     fss = {}
     partitions = {}
     parteds = {}
@@ -175,16 +181,27 @@ def generate_spaces(nodes):
     lvs = {}
 
     for mac in nodes.keys():
+        disk = DISKS[mac][0]
+
+        parteds[mac], partitions[mac] = make_parted_and_partitions(disk)
         fss[mac] = deepcopy(FS)
-        partitions[mac] = deepcopy(PARTITION)
-        parteds[mac] = deepcopy(PARTED)
-        pvs[mac] = deepcopy(PV)
-        vgs[mac] = deepcopy(VG)
-        lvs[mac] = deepcopy(LV)
+        pvs[mac] = make_pv(disk)
+        vgs[mac] = make_vg(pvs[mac].values())
+        vg = vgs[mac].values()[0]
+        lvs[mac] = make_lv(vg)
 
     return fss, partitions, parteds, pvs, vgs, lvs
 
 
-NODES, DISKS = generate_nodes_disks(2)
+NODES = {}
+DISKS = {}
 
-FSS, PARTITIONS, PARTEDS, PVS, VGS, LVS = generate_spaces(NODES)
+FSS = {}
+PARTITIONS = {}
+PARTEDS = {}
+PVS = {}
+VGS = {}
+LVS = {}
+NODES, DISKS = get_nodes_and_disks()
+
+FSS, PARTITIONS, PARTEDS, PVS, VGS, LVS = generate_spaces(NODES, DISKS)
