f78f2d2edd5c8f6ee249c3be4ed7b3d949c80b85
Evgeniy L <eli@mirantis.com>
2015-10-28 16:53:09 +0300

    Add working partitioning schema

--- bareon_api/models.py
+++ bareon_api/models.py
@@ -56,25 +56,38 @@ class VolumeGroup(JsonifyMixin, objects.VG):
 FS = {
     # ???(prmtl) now we identify file system by lablel
     # but should be considered to find a better way
-    'kogut': FileSystem(
-        device='/dev/kurnik/kogut',
-        fs_label='kogut',
+    'boot': FileSystem(
+        device='/dev/sda3',
+        fs_label='boot',
         fs_type='ext2',
-        mount='/tmp/kogutek'
+        mount='/boot'
     ),
+    'root': FileSystem(
+        device='/dev/mapper/os-root',
+        fs_label='root',
+        fs_type='ext4',
+        mount='/'
+    ),
+    'swap': FileSystem(
+        device='/dev/mapper/os-swap',
+        fs_label='',
+        fs_type='swap',
+        mount='swap'
+    ),
+
 }
 
 
 def make_lv(vg):
     return {
-        'kura': LogicalVolume(
-            name='kura',
-            size=69,
+        'root': LogicalVolume(
+            name='root',
+            size=10000,
             vgname=vg.name,
         ),
-        'kogut': LogicalVolume(
-            name='kogut',
-            size=96,
+        'swap': LogicalVolume(
+            name='swap',
+            size=2000,
             vgname=vg.name,
         )
     }
@@ -82,8 +95,8 @@ def make_lv(vg):
 
 def make_vg(pvs):
     return {
-        'kurnik': VolumeGroup(
-            name='kurnik',
+        'os': VolumeGroup(
+            name='os',
             pvnames=[pv.name for pv in pvs],
         )
     }
@@ -97,9 +110,52 @@ def make_parted_and_partitions(disk):
                 name='/dev/{0}1'.format(device),
                 device='/dev/{0}'.format(device),
                 count=1,
-                partition_type='primary',
                 begin=1,
-                end=20000,
+                end=25,
+                guid=None,
+                configdrive=False,
+                flags=['bios_grub'],
+                partition_type='primary',
+            ),
+            '{0}2'.format(device): Partition(
+                name='/dev/{0}2'.format(device),
+                device='/dev/{0}'.format(device),
+                count=2,
+                begin=25,
+                end=225,
+                guid=None,
+                configdrive=False,
+                partition_type='primary',
+            ),
+            '{0}3'.format(device): Partition(
+                name='/dev/{0}3'.format(device),
+                device='/dev/{0}'.format(device),
+                count=3,
+                begin=225,
+                end=425,
+                guid=None,
+                configdrive=False,
+                partition_type='primary',
+            ),
+            '{0}4'.format(device): Partition(
+                name='/dev/{0}4'.format(device),
+                device='/dev/{0}'.format(device),
+                count=4,
+                begin=425,
+                end=20045,
+                guid=None,
+                configdrive=False,
+                partition_type='primary',
+            ),
+            '{0}5'.format(device): Partition(
+                name='/dev/{0}5'.format(device),
+                device='/dev/{0}'.format(device),
+                count=5,
+                begin=20045,
+                end=20445,
+                guid=None,
+                configdrive=True,
+                partition_type='primary',
             )
         }
     }
@@ -107,7 +163,9 @@ def make_parted_and_partitions(disk):
         disk['device']: Parted(
             label='gpt',
             name='/dev/{0}'.format(disk['device']),
-            partitions=partitions[device].values()
+            # Partitions should be created in specific order
+            partitions=sorted(partitions[device].values(), key=lambda v: v.count),
+            install_bootloader=True
         )
 
     }
@@ -116,8 +174,8 @@ def make_parted_and_partitions(disk):
 
 def make_pv(disk):
     return {
-        '{}1'.format(disk['device']): PhysicalVolume(
-            name='/dev/{0}1'.format(disk['device']),
+        '{}4'.format(disk['device']): PhysicalVolume(
+            name='/dev/{0}4'.format(disk['device']),
             metadatacopies=2,
             metadatasize=28,
         )
