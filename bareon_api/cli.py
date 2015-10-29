af30917c8a8ff01614fc629a21847356d33092e1
Evgeniy L <eli@mirantis.com>
2015-10-29 15:57:13 +0300

    Sync data on app start

--- bareon_api/cli.py
+++ bareon_api/cli.py
@@ -12,7 +12,15 @@ from bareon_api.data_sync import sync_all_nodes
 current_dir = os.path.dirname(__file__)
 
 
+def data_sync():
+    resp = requests.post('http://{host}:{port}/v1/actions/sync_all'.format(
+        host=CONF.host_ip, port=CONF.port))
+    print('Response code {0}'.format(resp.status_code))
+    print(resp.text)
+
+
 def run():
+    sync_all_nodes()
     prop_dir = os.path.join(current_dir, '..', 'etc', 'bareon')
 
     application = deploy.loadapp(
@@ -21,10 +29,3 @@ def run():
         relative_to='.')
 
     httpserver.serve(application, host=CONF.host_ip, port=CONF.port)
-
-
-def data_sync():
-    resp = requests.post('http://{host}:{port}/v1/actions/sync_all'.format(
-        host=CONF.host_ip, port=CONF.port))
-    print('Response code {0}'.format(resp.status_code))
-    print(resp.text)
